fe3dc13a97c23a23844e952a24ad5a66a97930ca
Patrick Sajous <sajous@pop-os.localdomain>
2022-05-22 00:25:01 -0300

    Pesonalizado Pagina de Login

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'crispy_forms',
+    'users',
+
 
     'allauth',
     'allauth.account',
@@ -122,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -135,6 +141,9 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+RISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 SITE_ID = 1
 
